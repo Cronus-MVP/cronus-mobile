846dd550aaca1fb873bc9f4817ee4944f7b0e210
marzouk Baig <mbaig2@u.rochester.edu>
2021-04-12 19:25:52 -0400

    fixed logout colors

--- src/screens/HomeScreen/VendorHomeScreen/styles.js
+++ src/screens/HomeScreen/VendorHomeScreen/styles.js
@@ -30,7 +30,7 @@ export default StyleSheet.create({
     button: {
         height: 47,
         borderRadius: 5,
-        backgroundColor: '#788eec',
+        backgroundColor: '#ffba3b',
         width: 80,
         alignItems: "center",
         justifyContent: 'center'
